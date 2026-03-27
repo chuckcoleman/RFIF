c859d6b5594a72ac2e9c296a6b6f811ac6df9ecb
chuckcoleman <chuckcoleman@yahoo.com>
2026-03-27 11:26:35 -0400

    Final polish

--- src/rfif_r.h
+++ src/rfif_r.h
@@ -1,18 +1,9 @@
 #ifndef RFIF_R_H
 #define RFIF_R_H
 
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunknown-warning-option"
-#endif
-
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Print.h>
 #include <R_ext/Error.h>
-
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
 
 #endif
